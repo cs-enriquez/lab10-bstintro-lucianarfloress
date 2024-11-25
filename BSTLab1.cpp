510727a72b93be921a844985762d0d828ece1382
lucianarfloress <161652649+lucianarfloress@users.noreply.github.com>
2024-11-25 05:35:04 +0000

    .

--- BSTLab1.cpp
+++ BSTLab1.cpp
@@ -22,23 +22,23 @@ class Node {
 // {13, 0, 7, 6, 21, 15, -2, 12, 99, 18, 19, -1}
 Node* createTree() {
 	// root
-	Node *root = nullptr;
+	Node *root = new Node(13);
 
-	// level 1 (children of root)
+			// LEFT SIDE
+	root -> left = new Node(0);
+	root -> left -> left = new Node(-2);
+	root -> left -> right = new Node(7);
+	root -> left -> right -> left = new Node(6);
+	root -> left -> right -> right = new Node(12);
+	root -> left -> left -> right = new Node(-1);
 
-	// level 2 (children of 0)
-
-	// level 2 (children of 21)
-
-	// level 3 (children of -2)
-
-	// level 3 (children of 7)
-
-	// level 3 (children of 15)
-
-	// level 3 (children of 99)
-
-	// level 4 (children of 18)
+			// RIGHT SIDE
+	root -> right = new Node(21);
+	root -> right -> left = new Node(15);
+	root -> right -> right = new Node(99);
+	root -> right -> left -> right = new Node(18);
+	root -> right -> left -> right -> right = new Node(19);
+	
 
 	return root;
 }
